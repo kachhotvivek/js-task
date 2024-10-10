a6f2893ced54c866da31309010c90605124c1d0c
Kachhot vivek <kachhotvivek18@gmail.com>
2024-10-10 16:17:47 +0530

    Add files via upload

---
+++ task_1.js
@@ -0,0 +1,59 @@
+// use to let
+let id = 18;
+let function1 = function() {
+    console.log(id);
+}
+function1();
+
+
+// use to const
+const name = "vivek";
+const function2 = function () {
+    console.log(name);
+}
+function2();
+
+// use to var
+var email = "viek@gmail.com"
+var function3 = function(){
+    console.log(email);
+}
+function3();
+
+// == is use to int number for comperition
+console.log(5 == 5);
+
+// === is use to one varivable value int and second variable value is string.
+console.log('5' === 5);
+
+// let use to function
+let fname = "dax"
+let function4 = function(){
+     fname = "vivek"
+     console.log(fname);
+
+}
+console.log(fname)
+function4()
+
+// const use to function
+const secondname = "ritik"
+const function5 = function(){
+     //secondname = "kuldeep"
+     console.log(secondname);
+
+}
+console.log(secondname)
+function5()
+
+
+// var use to function
+var lastname = "raj"
+var function6 = function(){
+     lastname = "ramesh"
+     console.log(lastname);
+
+}
+console.log(lastname)
+function6()
+
