b8b9ba301a626dadf4297d2735227a48775523df
Kachhot vivek <kachhotvivek18@gmail.com>
2024-10-18 16:33:31 +0530

    Add files via upload

---
+++ task_3.js
@@ -0,0 +1,24 @@
+// create in function 
+
+function user() {
+    const fullname = "   kachhot vivek   ";
+    const score = "1818";
+    
+    // Remove any leading or trailing spaces from the full name.
+    const trimmedName = fullname.trim();
+
+    // Convert the name to all uppercase letters.
+    const uppername = trimmedName.toUpperCase();
+
+    // Convert the score into a string with two decimal places.
+    const newscour =  Number(score);
+    const formetedscor = newscour.toFixed(2);
+
+    console.log(`Hello  ${uppername}, your score is ${formetedscor}  `);
+
+}
+console.log(user());
+
+
+
+
